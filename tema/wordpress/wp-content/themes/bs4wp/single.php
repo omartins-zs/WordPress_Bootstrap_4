94d9e13718875101dbbbbb89d9576764874bd0d0
omartins-zs <gabrielmatheus209@gmail.com>
2022-10-18 16:22:51 -0300

    Loop de post destaque personalizado na Front Page

--- tema/wordpress/wp-content/themes/bs4wp/single.php
+++ tema/wordpress/wp-content/themes/bs4wp/single.php
@@ -4,7 +4,6 @@
 
     <div class="col-md-8 col-sm-12">
 
-
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
         <?php the_post_thumbnail('post_thumbnail', array('class' => 'img-fluid rounded')) ?>
@@ -23,8 +22,6 @@
         <?php else : get_404_template();
         endif; ?>
 
-
-     
     </div>
 
     <?php get_sidebar(); ?>
