876bdfd9b9ba3c3135c7030b496561fee25c2100
omartins-zs <gabrielmatheus209@gmail.com>
2022-10-18 08:42:13 -0300

    Ajuste para executar Task default watch no Gulp 4

--- layout/gulpfile.js
+++ layout/gulpfile.js
@@ -1,15 +1,15 @@
-const gulp = require("gulp");
-const sass = require('gulp-sass')(require('sass'));
+var gulp = require("gulp");
+var sass = require('gulp-sass')(require('sass'));
 
 
-gulp.task('sass', function() {
+gulp.task('sass', gulp.series( function() {
     return gulp.src(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'])
     .pipe(sass()) // Converter o Sass em CSS
     .pipe(gulp.dest('src/css'));
-});
+}));
 
-gulp.task('watch', function(){
-    gulp.watch(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'],['sass']);
-});
+gulp.task('watch',  gulp.series( function(){
+    gulp.watch(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'], gulp.parallel(['sass']));
+}));
 
-gulp.task('default' ['sass', 'watch']);
+gulp.task('default', gulp.series(['sass','watch']));
