e8d36310ca3c16af8385d3afbfb35ad3538ef436
omartins-zs <gabrielmatheus209@gmail.com>
2022-10-26 15:42:25 -0300

    Criando pág. de erro (404) e de buscas (Search)

---
+++ tema/wordpress/wp-content/themes/bs4wp/404.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+
+<div class="row">
+
+    <div class="col-md-8 col-sm-12">
+
+        
+
+<h3 class="mb-3">Pagina não encontrada</h3>
+<p>o caminho para o conteudo que voce dseja acessar esta errado ou a pagina pode ter sido excluida.</p>
+<p>Experiemnte realizar uma busca para ecnontrar o conteudo que deseja.</p>
+    <?php get_sidebar(); ?>
+
+</div>
+
+</div>
+
+<?php get_footer(); ?>
